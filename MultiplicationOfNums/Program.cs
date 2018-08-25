ae1e44ce54f7f98bc0d42d7c203a3a9fa4d59613
Maken Hubbard <makhub26@outlook.com>
2018-08-25 10:52:03 -0500

    Add project files.

---
+++ MultiplicationOfNums/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiplicationOfNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
